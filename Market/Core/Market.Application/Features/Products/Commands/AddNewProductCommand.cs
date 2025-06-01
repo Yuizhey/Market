e6a16c2f4d26dab7434f66bb1bbb0d4c3627f8e2
Yuizhey <Gre4ka28@yandex.ru>
2025-06-01 22:43:30 +0300

    добавление поля

--- Market/Core/Market.Application/Features/Products/Commands/AddNewProductCommand.cs
+++ Market/Core/Market.Application/Features/Products/Commands/AddNewProductCommand.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Market.Application.Features.Products.Commands;
 
@@ -7,4 +8,5 @@ public class AddNewProductCommand : IRequest
     public required string Title { get; set; }
     public required string Text { get; set; }
     public decimal Price { get; set; }
+    public IFormFile? MainImage { get; set; }
 }
