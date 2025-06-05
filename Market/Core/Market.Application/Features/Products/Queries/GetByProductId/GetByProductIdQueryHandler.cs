134782585484fbbf7cfb5f59cc5cca4d7faa8244
Yuizhey <Gre4ka28@yandex.ru>
2025-06-05 20:37:35 +0300

    правки

--- Market/Core/Market.Application/Features/Products/Queries/GetByProductId/GetByProductIdQueryHandler.cs
+++ Market/Core/Market.Application/Features/Products/Queries/GetByProductId/GetByProductIdQueryHandler.cs
@@ -1,4 +1,5 @@
 using Market.Application.Interfaces.Repositories;
+using Market.Application.Interfaces.Services;
 using MediatR;
 
 namespace Market.Application.Features.Products.Queries.GetByProductId;
@@ -6,20 +7,34 @@ namespace Market.Application.Features.Products.Queries.GetByProductId;
 public class GetByProductIdQueryHandler : IRequestHandler<GetByProductIdQuery, GetByProductIdDto>
 {
     private readonly IProductRepository _productRepository;
+    private readonly IMinioService _minioService;
 
-    public GetByProductIdQueryHandler(IProductRepository productRepository)
+    public GetByProductIdQueryHandler(IProductRepository productRepository,
+        IMinioService minioService)
     {
         _productRepository = productRepository;
     }
     public async Task<GetByProductIdDto> Handle(GetByProductIdQuery request, CancellationToken cancellationToken)
     {
         var product = await _productRepository.GetProductByIdAsync(request.id);
+        string? imageUrl = null;
+        if (!string.IsNullOrEmpty(product.CoverImagePath))
+        {
+            try
+            {
+                imageUrl = await _minioService.GetCoverImageUrlAsync(product.CoverImagePath, cancellationToken);
+            }
+            catch (Exception)
+            {
+                imageUrl = "assets/img/520x400.png"; 
+            }
+        }
         return new GetByProductIdDto()
         {
             Title = product.Title,
             Price = product.Price,
             Text = product.Text,
-            ImageURL = product.CoverImagePath
+            ImageURL = imageUrl ?? "assets/img/520x400.png"
         };
     }
 }
--- Market/Market.MVC/Models/Items/SingleItemVM.cs
+++ Market/Market.MVC/Models/Items/SingleItemVM.cs
@@ -5,4 +5,5 @@ public sealed class SingleItemVM
     public required string Title { get; set; }
     public required string Text { get; set; }
     public decimal Price { get; set; }
+    public string? ImageURL { get; set; }
 }
